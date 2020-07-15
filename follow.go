9e9acef72c27be85f0b86342d4d8c65fdd898336
Paul Crane <paul@acheronsoft.com>
2020-07-15 04:44:17 -0500

    Initial Commit.

---
+++ follow.go
@@ -0,0 +1,1 @@
+// If passed a file name it will show the linked path. If no arguments it will scan directory for links and display their paths.
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Soulsbane/follow
+
+go 1.14
