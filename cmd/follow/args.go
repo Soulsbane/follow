5aef5734e1e148c9a2a018649acd095e630b3b35
Paul Crane <paul@acheronsoft.com>
2022-12-08 23:39:52 -0600

    Add description to CLI output

--- cmd/follow/args.go
+++ cmd/follow/args.go
@@ -5,3 +5,7 @@ type ProgramArgs struct {
 	Ugly     bool     `arg:"-u" default:"false" help:"Remove colorized output. Yes it's ugly."`
 	Hidden   bool     `arg:"-i" default:"false" help:"Show hidden files."`
 }
+
+func (args ProgramArgs) Description() string {
+	return "Follows all links to there destination and prints a nice readout of the paths."
+}
